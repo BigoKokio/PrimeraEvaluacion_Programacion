2c60ab49223de429ac16c51c847236bc51ad8008
NZXT <grando.smokio560@gmail.com>
2020-11-18 20:43:23 +0100

    Commit ejercicios sesión 7

---
+++ src/Clase/Sesión7/Ej1.java
@@ -0,0 +1,17 @@
+package Clase.Sesión7;
+
+import java.util.Scanner;
+
+public class Ej1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Introduce un número entre 1 y 100: ");
+        int numero = sc.nextInt();
+        while ((numero < 1) || (numero > 100)) {
+            System.out.println("El número " + numero + " no está en el rango introducido");
+            System.out.println("Introduce un número entre 1 y 100: ");
+            numero = sc.nextInt();
+        }
+        System.out.println("El número introducido es: " + numero);
+    }
+}
---
+++ src/Clase/Sesión7/Ej4.java
@@ -0,0 +1,10 @@
+package Clase.Sesión7;
+
+import java.util.Scanner;
+
+public class Ej4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int multip
+    }
+}
