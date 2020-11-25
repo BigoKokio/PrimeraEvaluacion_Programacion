68dab3035765b086ddd27e1350208392e8f0c318
Sergio Murias <sergio.murias@educa.madrid.org>
2020-11-25 16:18:14 +0100

    Commit añadiendo Ej5 y Ej6

--- src/Clase/Sesión7/Ej5.java
+++ src/Clase/Sesión7/Ej5.java
@@ -2,15 +2,18 @@ package Clase.Sesión7;
 
 import java.util.Scanner;
 
-public class Ej4 {
+public class Ej5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int multiplos = 5;
         int contMultiplos = 0;
-        while (contMultiplos < 20) {
+        while (contMultiplos < 50) {
             if (multiplos % 5 == 0) {
                 System.out.print(multiplos + "\t");
                 contMultiplos++;
+                if (contMultiplos % 10 == 0) {
+                    System.out.println("\t");
+                }
             }
             multiplos++;
         }
