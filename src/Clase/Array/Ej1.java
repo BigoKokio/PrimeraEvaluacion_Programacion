96790cf51b7619114ef0830a473c26997178c34d
Sergio Murias <sergio.murias@educa.madrid.org>
2020-12-01 17:07:59 +0100

    Commit cambio EJ1 y Ej2

--- src/Clase/Array/Ej1.java
+++ src/Clase/Array/Ej1.java
@@ -11,5 +11,11 @@ public class Ej1 {
             array[i] = (int) (Math.random() * 10 + 1);
         }
         System.out.println(Arrays.toString(array));
+        System.out.println("Elemento:   Cuadrado:   Cubo: ");
+        System.out.println("==========================");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + "\t\t\t" + (array[i] * array[i]) + "\t\t\t" + (array[i] * array[i] * array[i]));
+
+        }
     }
 }
---
+++ src/Clase/Array/Ej2.java
@@ -0,0 +1,21 @@
+package Clase.Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ej2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] cadenas = new String[5];
+        for (int i = 0; i < cadenas.length; i++) {
+            System.out.println("Introduce una cadena de texto: ");
+            cadenas[i] = sc.next();
+        }
+        //System.out.println(Arrays.toString(cadenas));
+        String[] inverso = new String[5];
+        for (int i = 0; i < inverso.length; i++) {
+            inverso[4 - i] = cadenas[i];
+        }
+        System.out.println(Arrays.toString(inverso));
+    }
+}
