985bf2edaa5511c6b933d777c9b7b827e08ce6f2
Sergio Murias <sergio.murias@educa.madrid.org>
2020-12-01 17:50:05 +0100

    Commit cambio Ej3

--- src/Clase/Array/Ej3.java
+++ src/Clase/Array/Ej3.java
@@ -19,25 +19,24 @@ public class Ej3 {
             System.out.printf("%.2f ", notas[i]);
         }
         System.out.println();
-        System.out.println("Media: ");
         for (int i = 0; i < notas.length; i++) {
             sumaMedia = sumaMedia + notas[i];
         }
         media = sumaMedia / 5;
-        System.out.println(media);
-        System.out.println("Nota mas alta: ");
+        System.out.printf("Media: %.2f ", media);
+        System.out.println();
         for (int i = 0; i < notas.length; i++) {
             if (notas[i] > notaMayor) {
                 notaMayor = notas[i];
             }
         }
-        System.out.println(notaMayor);
-        System.out.println("Nota mas alta: ");
+        System.out.printf("Nota mayor: %.2f ", notaMayor);
+        System.out.println();
         for (int i = 0; i < notas.length; i++) {
             if (notas[i] < notaMenor) {
                 notaMenor = notas[i];
             }
         }
-        System.out.println(notaMenor);
+        System.out.printf("Nota menor: %.2f ", notaMenor);
     }
 }
