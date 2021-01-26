9c235c9a4397f23240707c0c0ebe7326ed7f43fe
Sergio Murias <sergio.murias@educa.madrid.org>
2021-01-26 19:18:39 +0100

    Commit Ej1Robot

--- src/Programación/POO/ClasesYObjetos/Ej1Robot.java
+++ src/Programación/POO/ClasesYObjetos/Ej1Robot.java
@@ -4,13 +4,21 @@ public class Ej1Robot {
     String nombre;
     String modelo;
 
+    public Ej1Robot() {
+        this.nombre = "Anónimo";
+        this.modelo = "Desconocido";
+    }
+
     public Ej1Robot(String nombre, String modelo) {
         this.nombre = nombre;
         this.modelo = modelo;
     }
 
-    public Ej1Robot() {
-        this.nombre = "Anónimo";
-        this.modelo = "Desconocido";
+    public static void main(String[] args) {
+        Ej1Robot a = new Ej1Robot();
+        System.out.println("El primer robot se llama " + a.nombre + " y pertenece al modelo " + a.modelo);
+
+        Ej1Robot b = new Ej1Robot("Wall-E", "DMZ24");
+        System.out.println("El segundo robot se llama " + b.nombre + " y pertenece al modelo " + b.modelo);
     }
 }
