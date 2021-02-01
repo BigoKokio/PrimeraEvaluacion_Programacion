dc7b8e9408604c6ebb9734408e4c526735eae78c
Big Smoke <grando.smokio560@gmail.com>
2021-02-01 17:21:06 +0100

    Ej08 Arrays2

--- src/Programación/EjerciciosArray2/Ej08.java
+++ src/Programación/EjerciciosArray2/Ej08.java
@@ -1,7 +1,27 @@
 package Programación.EjerciciosArray2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ej08 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (i * 5) + 5;
+        }
+        System.out.println(Arrays.toString(array));
+        int pos;
+        do {
+            System.out.println("Introduce posición: ");
+            pos = sc.nextInt();
+        } while (pos < 0 || pos > array.length - 1);
+        System.out.println("Introduce número a insertar: ");
+        int num = sc.nextInt();
+        for (int i = array.length - 2; i >= pos - 1; i--) {
+            array[i + 1] = array[i];
+        }
+        array[pos - 1] = num;
+        System.out.println(Arrays.toString(array));
     }
 }
