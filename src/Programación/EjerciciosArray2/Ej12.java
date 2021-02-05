23f97c3fb0fed98614a462d4ce6c560db54c6458
Sergio Murias <sergio.murias@educa.madrid.org>
2021-02-05 16:13:33 +0100

    Ejercicios 12 y 13 Arrays 2

--- src/Programación/EjerciciosArray2/Ej12.java
+++ src/Programación/EjerciciosArray2/Ej12.java
@@ -1,10 +1,25 @@
 package Programación.EjerciciosArray2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ej12 {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 1000);
+        }
+        System.out.println(Arrays.toString(array));
+        int pos;
+        do {
+            System.out.println("Introduce la posición a eliminar (0-9): ");
+            pos = sc.nextInt();
+        } while (pos < 0 || pos > 9);
+        for (int i = pos; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[array.length - 1] = 0;
+        System.out.println(Arrays.toString(array));
     }
 }
